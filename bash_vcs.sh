149b6228b11eec9370b357003a3341b337975e89
jwirman <jwirman@gmail.com>
2016-08-25 14:41:36 -0500

    new git working directory clean message

--- bash_vcs.sh
+++ bash_vcs.sh
@@ -75,7 +75,7 @@ function parse_git_branch {
 }
 
 function check_git_changes {
-    var=`git status 2> /dev/null | sed -e '/working directory clean$/!d' | wc -l`
+    var=`git status 2> /dev/null | sed -e '/working tree clean$/!d' | wc -l`
     if [ $var -ne 1 ]; then
         tput setaf 1 # red
     else
